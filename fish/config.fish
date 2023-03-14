006b53e059129faa8bac930630e372d7dab4e03a
ATH <alex.t.hoffmann@icloud.com>
2023-03-14 17:15:44 +0100

    updated

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
+
+set PATH /usr/local/Cellar/mtr/0.87/sbin $PATH
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
@@ -6,17 +8,7 @@ end
 # The original version is saved in /Users/alexanderhoffmann/.config/fish/config.fish.pysave
 set -x PATH "/Library/Frameworks/Python.framework/Versions/3.10/bin" "$PATH"
 
-# Setting PATH for Python 3.10
-# The original version is saved in /Users/alexanderhoffmann/.config/fish/config.fish.pysave
-set -x PATH "/Library/Frameworks/Python.framework/Versions/3.10/bin" "$PATH"
+alias pn="pnpm"
 
-# pnpm
-set -gx PNPM_HOME "/Users/alexanderhoffmann/Library/pnpm"
-set -gx PATH "$PNPM_HOME" $PATH
-# pnpm end
-pyenv init - | source
 
-# Bun
-set -Ux BUN_INSTALL "/Users/alexanderhoffmann/.bun"
-set -px --path PATH "/Users/alexanderhoffmann/.bun/bin"
 
